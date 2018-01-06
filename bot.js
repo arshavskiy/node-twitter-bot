505d4ac202ac7e1a9699333c84ca7c67e4f4b1fd
arshavskiy <pasha.websbee@gmail.com>
2018-01-06 23:57:31 +0200

    dynamic try

--- bot.js
+++ bot.js
@@ -3,26 +3,49 @@ const config = require('./config');
 const fs = require('fs');
 
 let T = new Twit(config);
+//
+// function getMePosts(t) {
+//     let params = {
+//         q : t  ,
+//         count: 100
+//     }
+//     T.get('search/tweets', params, gotData);
+//
+//     function gotData(err, data, response) {
+//         let tweets = data.statuses;
+//         console.log(data.statuses);
+//
+//         for (var i = 0, l =  tweets.length; i < l; i++) {
+//             console.log('we found ' + i + ' result' + ' ' + tweets[i].text + ' ' +tweets[i].user.created_at);
+//             fs.appendFile('./found/ufo.txt', tweets[i].user.name + ' \r: ' + tweets[i].text + '-- \r' + tweets[i].user.created_at + '\r\n', 'utf8', (err) => {
+//                 if (err) throw err;
+//         });
+//         }
+//     }
+// }
+// getMePosts('ufo since:2017-01-01')
 
-setInterval(getMePosts, 1000 * 60);
+setInterval( getMePosts , 1000 * 10);
 
-getMePosts();
+
+
 
 function getMePosts() {
     let params = {
-        q: 'ufo',
+        q: 'ufo since:2017-01-01',
         count: 100
     }
     T.get('search/tweets', params, gotData);
 
     function gotData(err, data, response) {
         let tweets = data.statuses;
+        console.log(data.statuses);
+
         for (var i = 0, l =  tweets.length; i < l; i++) {
-            console.log('we found ' + i + ' result');
-            fs.appendFile('./found/ufo.html', tweets[i].text + '\r\n', 'utf8', (err) => {
+            console.log('we found ' + i + ' result' + ' ' + tweets[i].text);
+            fs.appendFile('./found/ufo.txt', tweets[i].user.name + ' \r: ' + tweets[i].text + '-- \r' + tweets[i].user.created_at + '\r\n', 'utf8', (err) => {
                 if (err) throw err;
-            });
+        });
         }
     }
 }
-
